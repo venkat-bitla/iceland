9cb47a0b7c80d5b1ca742f383a49664fb30dfda9
venkat <venkat@elasticfox.com>
2023-05-02 08:59:43 +0530

    removed first and last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  validates :full_name, length: { maximum: 50 }
+  validates :body, length: { maximum: 80 }
+  validate :valid_username
+
+  def valid_username
+    errors.add(:username, 'is already taken') if User.exists?(username:)
+    restricted_username_list = %(admin root dashboard analytics appearance settings preferences calendar)
+    errors.add(:username, 'is restricted') if restricted_username_list.include?(username)
+  end
+
 end
